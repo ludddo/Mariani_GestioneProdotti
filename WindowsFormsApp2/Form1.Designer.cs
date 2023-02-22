6a67b770fe7c7250a643d764ec1b64525901b865
Ludovico Mariani <mariani.ludovico.studente@itispaleocapa.it>
2023-02-22 13:56:04 +0100

    Aggiungere i file di progetto.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nome = new System.Windows.Forms.TextBox();
+            this.prezzo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.salva = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // nome
+            // 
+            this.nome.Location = new System.Drawing.Point(128, 84);
+            this.nome.Name = "nome";
+            this.nome.Size = new System.Drawing.Size(100, 20);
+            this.nome.TabIndex = 0;
+            this.nome.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // prezzo
+            // 
+            this.prezzo.Location = new System.Drawing.Point(128, 137);
+            this.prezzo.Name = "prezzo";
+            this.prezzo.Size = new System.Drawing.Size(100, 20);
+            this.prezzo.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(125, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nome";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(125, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Prezzo";
+            // 
+            // salva
+            // 
+            this.salva.Location = new System.Drawing.Point(128, 199);
+            this.salva.Name = "salva";
+            this.salva.Size = new System.Drawing.Size(75, 23);
+            this.salva.TabIndex = 4;
+            this.salva.Text = "Salva";
+            this.salva.UseVisualStyleBackColor = true;
+            this.salva.Click += new System.EventHandler(this.salva_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(470, 38);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(306, 384);
+            this.listView1.TabIndex = 5;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(828, 471);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.salva);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.prezzo);
+            this.Controls.Add(this.nome);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox nome;
+        private System.Windows.Forms.TextBox prezzo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button salva;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public struct Prodotto
+        {
+            public string nome;
+            public float prezzo;
+        }
+
+        public Prodotto [] prodotto;
+        public int dim;
+
+        public Form1()
+        {
+            InitializeComponent();
+            prodotto = new Prodotto[100];
+            dim = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void salva_Click(object sender, EventArgs e)
+        {
+            
+            prodotto[dim].nome = nome.Text;
+            prodotto[dim].prezzo = float.Parse(prezzo.Text);
+            dim++;
+            Visualizza(prodotto);
+        }
+
+        public void Visualizza(Prodotto[] prodotto)
+        {
+            listView1.Items.Clear();
+            for (int i = 0; i < dim; i++)
+            {
+                listView1.Items.Add(ProdString(prodotto[i]));
+            }
+            
+        }
+
+        public string ProdString(Prodotto prodotto)
+        {
+            return "Nome: " + prodotto.nome + " Prezzo: " + prodotto.prezzo.ToString();
+        }
+    }
+}
